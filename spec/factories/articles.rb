e865000b39b94e4ea6012074f3961eef9eb5543b
Stanly Lau <stanly@odd-e.com>
2016-06-17 12:35:52 +0800

    Added list articles test case and streamlined travis

---
+++ spec/factories/articles.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :article do
+      title "Why is sky blue?"
+      text "A clear cloudless day-time sky is blue because molecules in the air scatter blue light from the sun more than they scatter red light."
+  end
+end
